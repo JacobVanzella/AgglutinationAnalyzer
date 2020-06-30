b5e4d48982d32dfa9e6511c091e9c87f6e1d08e0
Jacob Vanzella <JacobVanzella@users.noreply.github.com>
2020-06-30 03:18:42 -0700

    Uses Entropy instead of Correlation
    Changed plug detector to find plug candidate frames using the image proccesing toolboxes entropy function instead of corr2 function. Improves accuray and improves running time by up to 24%.

--- PlugDetector/detectPlugs.m
+++ PlugDetector/detectPlugs.m
@@ -11,22 +11,16 @@ function plugs = detectPlugs(filename)
     %% Initialize Workspace
     % Creates video reader to read frames from video file
     reader = VideoReader(filename);
-    % Use first frame of video as template to compare future frames against
-    template = rgb2gray(readFrame(reader));
-    reader.CurrentTime = 0;
-    % Template auto-correlation coefficient
-    TMM = template - mean2(template);
-    templateACC = sum(sum(TMM.*TMM));
-    % Frame correlation to template values, indexed by frame number
-    corr2Values = zeros(1,int32(reader.FrameRate*reader.Duration));
-    % How close a frame needs to be to minCorr2 to be a candidate
-    candidateRatio = 1.1;
+    % Frame entropy, indexed by frame number
+    entropyValues = zeros(1,int32(reader.FrameRate*reader.Duration));
+    % How close a frame needs to be to min entropy to be a candidate
+    candidateRatio = .99;
     % Struct to contain plug frames and associated information
     plugs = struct( ...
         'plugID', {}, ...
         'frame', {}, ...
         'frameID', {}, ...
-        'corrCoef', {});
+        'entropy', {});
     % Plug ID
     plugID = 0;
     % New plug flag
@@ -35,28 +29,22 @@ function plugs = detectPlugs(filename)
     frameCount = 0;
     
     %% Detect Candidate Frames
-    % Iterate through video and find min correlation with template
+    % Iterate through video and find max entropy
     while hasFrame(reader)
         frameCount = frameCount + 1;
-        currentFrame = rgb2gray(readFrame(reader)); % Read frame in gray scale
-        
-        % Get cross correlation coefficient
-        CMM = currentFrame - mean2(currentFrame);
-        corr2CurrentFrame = sum(sum(TMM.*CMM))/sqrt(templateACC*sum(sum(CMM.*CMM)));
-        
-        corr2Values(frameCount) = corr2CurrentFrame; % Assign correlation value
+        entropyValues(frameCount) = entropy(rgb2gray(readFrame(reader))); % Assign entropy value
     end
     
-    % Set the minimum correlation value
-    minCorr2 = min(corr2Values);
+    % Set the maximum entropy value
+    maxEntropy = max(entropyValues);
     % Reset frame count
     frameCount = 0;
     
-    % Collect candidate frames, those that are within % of min correlation
-    for i = 1:size(corr2Values,2)
+    % Collect candidate frames, those that are within % of max entropy
+    for i = 1:size(entropyValues,2)
         frameCount = frameCount + 1;
         
-        if corr2Values(frameCount) <= minCorr2*candidateRatio
+        if entropyValues(frameCount) >= maxEntropy*candidateRatio
             if newPlug
                 plugID = plugID + 1;
                 newPlug = false;
@@ -65,7 +53,7 @@ function plugs = detectPlugs(filename)
             plugs(end+1).plugID = plugID;
             plugs(end).frame = read(reader,frameCount);
             plugs(end).frameID = frameCount;
-            plugs(end).corrCoef = corr2Values(frameCount);
+            plugs(end).entropy = entropyValues(frameCount);
         else
             newPlug = true;
         end
